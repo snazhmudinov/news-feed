0b613b8daff629ab775d4859240125547f8495e9
Sheroz Nazhmudinov <nazhmudinov@parkside.at>
2019-04-16 13:33:11 +0200

    Fixed the page loading

--- app/src/main/java/com/snazhmudinov/newsfeed/api/ArticleDataSource.kt
+++ app/src/main/java/com/snazhmudinov/newsfeed/api/ArticleDataSource.kt
@@ -16,17 +16,21 @@ class ArticleDataSource(private val service: NewsService) : PageKeyedDataSource<
             .subscribeOn(Schedulers.io())
             .subscribe { articles, _ ->
                 totalRes = articles.totalResults
-                articles?.articles?.let { callback.onResult(it, null, FIRST_PAGE) }
+                articles?.articles?.let {
+                    callback.onResult(it, null, FIRST_PAGE)
+                }
             }
     }
 
     @SuppressLint("CheckResult")
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Article>) {
-        service.getTopUSHeadlines(page = params.key)
+        service.getTopUSHeadlines(page = params.key + 1)
             .subscribeOn(Schedulers.io())
             .subscribe { articles, _ ->
                 val adjacentKey = if (params.key < totalRes) params.key + 1 else null
-                articles?.articles?.let { callback.onResult(it, adjacentKey) }
+                articles?.articles?.let {
+                    callback.onResult(it, adjacentKey)
+                }
             }
     }
 
@@ -36,7 +40,9 @@ class ArticleDataSource(private val service: NewsService) : PageKeyedDataSource<
             .subscribeOn(Schedulers.io())
             .subscribe { articles, _ ->
                 val adjacentKey = if (params.key > 1) params.key - 1 else null
-                articles?.articles?.let { callback.onResult(it, adjacentKey) }
+                articles?.articles?.let {
+                    callback.onResult(it, adjacentKey)
+                }
             }
     }
 }
